0000638a7c5cf19142ab24bdddb16501d4a57779
Pat Lang <148596462+ArtemisTowel@users.noreply.github.com>
2024-06-07 22:18:17 -0500

    identified simplified solution for rps project

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
-<script>
-    console.log("Hello World!")
-</script>
+var items = ["rock", "paper", "scissors"];
+var getResult = function(a, b){
+    if (a === -1){
+        return "I said rock, paper, scissors!";
+    }
+    s = ["Player 1 wins","Player 2 wins","Draw"][(a-b+2)%3];
+    return s + " ( "+items[a]+" vs. "+items[b]+")";
+}
+console.log(getResult(items.indexOf(prompt("Pick rock, paper, or scissors.")),
+Math.floor(Math.random()*3)));
+
