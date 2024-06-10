0869794f649eb9dd61380a925732b6bb41228524
Pat Lang <148596462+ArtemisTowel@users.noreply.github.com>
2024-06-10 14:29:41 -0500

    saving between the round and game portion of rps.

--- js.js
+++ js.js
@@ -1,11 +1,69 @@
-var items = ["rock", "paper", "scissors"];
-var getResult = function(a, b){
-    if (a === -1){
-        return "I said rock, paper, scissors!";
-    }
-    s = ["Player 1 wins","Player 2 wins","Draw"][(a-b+2)%3];
-    return s + " ( "+items[a]+" vs. "+items[b]+")";
-}
-console.log(getResult(items.indexOf(prompt("Pick rock, paper, or scissors.")),
-Math.floor(Math.random()*3)));
+function getHumanChoice(){
+return prompt("Rock, Paper, Scissors?").toLowerCase()
+    console.log(getHumanChoice())}
 
+function getComputerChoice(){
+  let a = Math.random()
+    if (a<=.34){
+        return "rock"
+    }
+    else if (a<=.67){
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+    console.log(getComputerChoice())}
+
+    let humanScore=0
+    let computerScore=0
+
+
+
+function playRound(humanChoice, computerChoice){
+    
+    if(humanChoice === computerChoice){
+        return "It's a Draw!"
+    }
+
+    if(humanChoice === "rock"){
+        if(computerChoice === "scissors"){
+            ++humanScore
+            return "Rock beats Scissors! You Win!"
+           
+        }
+        else {
+            ++computerScore
+            return "Paper beats Rock! You Lose!"
+        }
+    }
+
+    if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            ++humanScore
+            return "Paper beats Rock! You Win!"
+        }
+        else {
+            ++computerScore
+            return "Scissors beats Paper! You Lose"
+        }
+    }
+
+    if(humanChoice === "scissors"){
+        if(computerChoice === "paper"){
+            ++humanScore
+            return "Scissors beats Paper! You Win!"
+        }
+        else {
+            ++computerScore
+            return "Rock beats Scissors! You Lose!"
+        }
+    }
+
+}
+
+
+
+console.log(playRound(getHumanChoice(),getComputerChoice()))
+
+console.log(humanScore, computerScore)
